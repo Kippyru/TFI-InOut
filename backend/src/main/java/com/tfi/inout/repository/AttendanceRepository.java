7aaa904e842fd186a229034459c70e04cecc61b6
Kevin <kfornier@gmail.com>
2026-04-07 19:26:42 -0300

    feat(repository): add Repositories for User, Role, Employee, Attendance and EventAttendance

---
+++ backend/src/main/java/com/tfi/inout/repository/AttendanceRepository.java
@@ -0,0 +1,9 @@
+package com.tfi.inout.repository;
+
+import com.tfi.inout.model.Attendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+}
---
+++ backend/src/main/java/com/tfi/inout/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.tfi.inout.repository;
+
+import com.tfi.inout.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}

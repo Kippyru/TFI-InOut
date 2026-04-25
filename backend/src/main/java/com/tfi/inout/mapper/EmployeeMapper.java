3e9636ce924fd2a6948ed0600fbd13c8a7e258b8
Kevin <kfornier@gmail.com>
2026-04-25 20:48:06 -0300

    Update mappers for Employee and User

--- backend/src/main/java/com/tfi/inout/mapper/EmployeeMapper.java
+++ backend/src/main/java/com/tfi/inout/mapper/EmployeeMapper.java
@@ -2,19 +2,36 @@ package com.tfi.inout.mapper;
 
 import com.tfi.inout.dto.EmployeeDto;
 import com.tfi.inout.model.Employee;
+import com.tfi.inout.model.User;
+import com.tfi.inout.repository.UserRepository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface EmployeeMapper {
+public abstract class EmployeeMapper {
+    @Autowired
+    protected UserRepository userRepository;
 
-    Employee toEntity(EmployeeDto employeeDto);
+    @Mapping(target = "user", source = "user", qualifiedByName = "mapIdToUser")
+    public abstract Employee toEntity(EmployeeDto employeeDto);
 
-    EmployeeDto toDto(Employee employee);
+    @Mapping(target = "user", source = "user.id")
+    public abstract EmployeeDto toDto(Employee employee);
 
-    List<EmployeeDto> toList(List<Employee> employeeList);
+    @Named("mapIdToUser")
+    protected User mapIdToUser(Long user) {
+        if (user == null) return null;
+        return userRepository.findById(user)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 
-    void updateEmployee(EmployeeDto employeeDto, @MappingTarget Employee employee);
+    public abstract List<EmployeeDto> toList(List<Employee> employeeList);
+
+    @Mapping(target = "user", source = "user", qualifiedByName = "mapIdToUser")
+    public abstract void updateEmployee(EmployeeDto employeeDto, @MappingTarget Employee employee);
 }
--- backend/src/main/java/com/tfi/inout/mapper/UserMapper.java
+++ backend/src/main/java/com/tfi/inout/mapper/UserMapper.java
@@ -1,10 +1,8 @@
 package com.tfi.inout.mapper;
 
 import com.tfi.inout.dto.UserDto;
-import com.tfi.inout.model.Employee;
 import com.tfi.inout.model.Role;
 import com.tfi.inout.model.User;
-import com.tfi.inout.repository.EmployeeRepository;
 import com.tfi.inout.repository.RoleRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -17,24 +15,13 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public abstract class UserMapper {
     @Autowired
-    protected EmployeeRepository employeeRepository;
-    @Autowired
     protected RoleRepository roleRepository;
 
-    @Mapping(target = "employee", source = "employee", qualifiedByName = "mapIdtoEmployee")
     @Mapping(target = "role", source = "role", qualifiedByName = "mapIdtoRole")
     public abstract User toEntity(UserDto  userDto);
 
-    @Mapping(target = "employee", source = "employee.id")
     @Mapping(target = "role", source = "role.id")
     public abstract UserDto toDto(User user);
-
-    @Named("mapIdtoEmployee")
-    protected Employee mapIdtoEmployee(Long employee) {
-        if (employee == null) return null;
-        return employeeRepository.findById(employee)
-                .orElseThrow(() -> new RuntimeException("Employee not found"));
-    }
 
     @Named("mapIdtoRole")
     protected Role mapIdtoRole(Long role) {
@@ -43,7 +30,6 @@ public abstract class UserMapper {
                 .orElseThrow(() -> new RuntimeException("Role not found"));
     }
 
-    @Mapping(target = "employee", source = "employee", qualifiedByName = "mapIdtoEmployee")
     @Mapping(target = "role", source = "role", qualifiedByName = "mapIdtoRole")
     public abstract void updateUser(UserDto userDto, @MappingTarget User user);
 

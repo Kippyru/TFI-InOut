cd90ff447ab21d12f1eef50790e45c4b026c50c1
Kevin <kfornier@gmail.com>
2026-05-19 20:36:33 -0300

    feat(EventAttendance): implement schedule aware attendance registration

--- backend/src/main/java/com/tfi/inout/repository/EventAttendanceRepository.java
+++ backend/src/main/java/com/tfi/inout/repository/EventAttendanceRepository.java
@@ -4,10 +4,13 @@ import com.tfi.inout.model.EventAttendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EventAttendanceRepository extends JpaRepository<EventAttendance, Long> {
-    Optional<EventAttendance>
-    findTopByEmployeeIdOrderByHourDesc(Long employeeId);
+    List<EventAttendance> findByEmployeeIdAndDate(Long employeeId, LocalDate date);
+
+    Optional<EventAttendance> findByEmployeeIdAndDateAndEventType(Long employeeId, LocalDate date, String eventType);
 }

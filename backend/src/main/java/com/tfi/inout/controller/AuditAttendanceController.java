1dee27e26b17a60c079a5e28748847b9828246bd
Kevin <kfornier@gmail.com>
2026-06-02 17:35:37 -0300

    feat(cors): enable cross-origin requests for controllers

--- backend/src/main/java/com/tfi/inout/controller/AuditAttendanceController.java
+++ backend/src/main/java/com/tfi/inout/controller/AuditAttendanceController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/admin/attendance")
 @RequiredArgsConstructor

578372fb5d568fbd0ede7fa8f693b5b98864bade
Kevin <kfornier@gmail.com>
2026-06-02 17:32:08 -0300

    feat: update routes, styles and main

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/login/login';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'login', component: Login },
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard')
+        .then(m => m.Dashboard),
+    canActivate: [authGuard]
+  },
+
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+
+];

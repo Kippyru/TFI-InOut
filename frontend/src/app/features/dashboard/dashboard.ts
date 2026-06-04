6d7a050579ea6eba6c790ad3ec232e2898b74d78
Kevin <kfornier@gmail.com>
2026-06-04 19:42:21 -0300

    feat: update the dashboard

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { Token } from '../../core/storage/token';
+import { MaterialModule } from '../../shared/ui/materials-module';
+import { DrawerMenu } from '../../shared/components/drawer-menu/drawer-menu';
+import { MatDrawer } from '@angular/material/sidenav';
+import { ScreenSize } from '../../core/services/screen-size';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MaterialModule, RouterOutlet, DrawerMenu],
   templateUrl: './dashboard.html',
-  styleUrl: '../dashboard/dashboard.css',
+  styleUrl: '../dashboard/dashboard.scss',
 })
 export class Dashboard {
+
+  @ViewChild('drawer') drawer!: MatDrawer;
+  private readonly screenSizeService = inject(ScreenSize);
+
+  isMobile = this.screenSizeService.isMobile;
 
   constructor(
     private token: Token,

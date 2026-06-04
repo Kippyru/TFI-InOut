2fe09ac911e916d8467dd3238cd1d889d256241c
Kevin <kfornier@gmail.com>
2026-06-04 19:10:16 -0300

    add footer, setting and menu modules

---
+++ frontend/src/app/shared/components/drawer-menu/drawer-menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawer-menu',
+  imports: [],
+  templateUrl: './drawer-menu.html',
+  styleUrl: './drawer-menu.css',
+})
+export class DrawerMenu {}

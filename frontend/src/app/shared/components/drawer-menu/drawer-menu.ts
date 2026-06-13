6a35d22e4f0dde361c7520ccdc07da0d49268204
Kevin <kfornier@gmail.com>
2026-06-13 07:42:37 -0300

    feat: now some buttons are only visible for the admin

--- frontend/src/app/shared/components/drawer-menu/drawer-menu.ts
+++ frontend/src/app/shared/components/drawer-menu/drawer-menu.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../ui/materials-module';
+import { UserService } from '../../../core/services/user.service';
 
 @Component({
   selector: 'app-drawer-menu',
   standalone: true,
-  imports: [RouterModule, MaterialModule],
+  imports: [CommonModule, RouterModule, MaterialModule],
   templateUrl: './drawer-menu.html',
   styleUrl: './drawer-menu.css',
 })
-export class DrawerMenu {}
+export class DrawerMenu implements OnInit {
+  isAdmin = false;
+
+  constructor(private userService: UserService, private cdr: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    this.userService.getMe().subscribe({
+      next: (user) => {
+        this.isAdmin = user.role === 1;
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+}

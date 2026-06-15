bcaa4fb0661913f8b9e85d27d56d6f5c6b4df1c3
Kevin <kfornier@gmail.com>
2026-06-14 23:10:21 -0300

    chore: change css to scss

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AuthService } from '../../../core/services/auth-service';
 import { Token } from '../../../core/storage/token';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MaterialModule } from '../../../shared/ui/materials-module';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, MaterialModule],
   templateUrl: './login.html',
-  styleUrls: ['../login/login.css'],
+  styleUrls: ['../login/login.scss'],
 })
 export class Login {
 
   username: string = '';
   password: string = '';
-  errorMessage: string = '';
+  errorMessage = signal('');
 
   constructor(
     private authService: AuthService,
@@ -23,6 +24,12 @@ export class Login {
   ) { }
 
   login() {
+    if (!this.username || !this.password) {
+      this.errorMessage.set('Por favor, complete todos los campos');
+      return; 
+    }
+
+    this.errorMessage.set('');
 
     const loginDto = {
       username: this.username,
@@ -31,15 +38,13 @@ export class Login {
 
     this.authService.login(loginDto).subscribe({
       next: (data) => {
-
         this.Token.saveToken(data.token);
-
         console.log("Token guardado:", data.token);
-
         this.router.navigate(['/dashboard']);
       },
-      error: () => {
-        this.errorMessage = 'Usuario o contraseña incorrectos';
+      error: (err) => {
+        this.errorMessage.set('Usuario o contraseña incorrectos');
+        console.error('Error de login:', err);
       }
     });
   }

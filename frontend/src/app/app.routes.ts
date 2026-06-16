f73bda2977f75e43eee6c47b1d77748930112464
Kevin <kfornier@gmail.com>
2026-06-16 01:08:35 -0300

    chore: add path to redirect into login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
 
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
   { path: 'login', component: Login },
 
   {
